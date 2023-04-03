67c6858401f204d81218d5f67853c62976efa96c
Norbert Lazzeri <norbert.lazzeri@firstbird.com>
2023-04-03 21:10:08 +0200

    lots of stuff, also moving to m2

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -4,6 +4,7 @@ alias vim="nvim"
 alias v="nvim"
 
 # aws sso + export profile for legacy aws tools
+# https://github.com/victorskl/yawsso
 sso() {
   aws sso login --profile $1 && yawsso --profile $1
 }
@@ -17,3 +18,9 @@ repos() {
 alias tail-lsp="tail -f $HOME/.local/state/nvim/lsp.log"
 alias tail-nvim-metals="tail -f $HOME/.cache/nvim/nvim-metals/nvim-metals.log"
 alias tail-bsp="tail -f $HOME/Library/Caches/org.scalameta.metals/bsp.trace.json"
+
+# aws bastion host dont know alacritty TERM yet. this will set it to a more commonly known TERM 
+alias ssh="TERM=xterm-256color $(which ssh)"
+tg() {
+  terragrunt $@
+}
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,3 +1,4 @@
+export PATH=/opt/homebrew/bin:$PATH
 #using zap
 #https://github.com/zap-zsh/zap
 [ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"
@@ -20,3 +21,6 @@ plug "zap-zsh/fzf" #enable reverse search with fzf
 #plug "zap-zsh/vim" #vim like navigation in shell and suggestion menus
 plug "zsh-users/zsh-syntax-highlighting" 
 
+
+autoload -U +X bashcompinit && bashcompinit
+complete -o nospace -C /usr/local/bin/terraform terraform
